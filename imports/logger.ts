2e64b32a63bc56e0dbde57b3d3769b2d5c19c3fc
The_Profaned <40006494+The-Profaned@users.noreply.github.com>
2026-01-29 16:11:31 -0500

    Delete imports/logger.ts

--- imports/logger.ts
+++
@@ -1,9 +0,0 @@
-// imports
-import { State } from './types.js';
-
-// Logger function to log messages based on type and state settings
-export const logger = (state: State, type: 'all' | 'debug', source: string, message: string, // Logs type + source of the message + the message itself
-    ): void => {const logMessage = `[${source}] ${message}`;
-        if (type === 'all') bot.printGameMessage(logMessage); // Always log 'all' messages to game chat
-        if (type === 'all' || (type === 'debug' && state.debugEnabled)) bot.printLogMessage(logMessage); // Log 'debug' messages to log only if debug is enabled in state
-    };
