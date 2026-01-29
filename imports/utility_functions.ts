793b386879d9a489e39a82a72c040cc435ada6ca
The_Profaned <40006494+The-Profaned@users.noreply.github.com>
2026-01-29 16:12:38 -0500

    Delete imports/utility_functions.ts

--- imports/utility_functions.ts
+++
@@ -1,8 +0,0 @@
-// Utility functions
-export const utilityFunctions = {
-
-    // random integer between min and max (inclusive)
-    randInt: (min: number, max: number): number =>
-        Math.floor(Math.random() * (max - min + 1)) + min,
-
-};
